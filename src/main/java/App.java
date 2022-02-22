cd1280b4edf43fafaa1325b14877d5af052aaa1c
Roman Torop <toroprom4ik@gmail.com>
2022-02-22 16:25:24 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean1 == bean2);
+
+        Cat cat1 = applicationContext.getBean("cat",Cat.class);
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat1 == cat2);
+    }
+}
